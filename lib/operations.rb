7fbe639b5aca964ec08bd629b0116a60a8d8a4b8
Ho Yin Keung <hykeung92@gmail.com>
2019-12-10 19:03:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-  speed > 60 || speed < 40 ? true : FALSE
-  return true
+
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : FALSE
+  speed <= 30 || speed >= 95 ? true : FALSE
+  speed = 50 ? FALSE : true
 end
 	
 
