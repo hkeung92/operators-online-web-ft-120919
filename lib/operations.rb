a5d871e0022cbdc3689db4d22ef3889611dc1016
Ho Yin Keung <hykeung92@gmail.com>
2019-12-10 19:11:18 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,17 @@
 def unsafe?(speed)
   if speed > 60
-    return "true" 
+    return true 
   elseif speed < 40
-    return "true"
+    return true
   else speed.between(40, 60)
-    return "false"
+    return false
 end
 
 
 
 def not_safe?(speed)
-	speed > 60 || speed < 40 ? true : FALSE
-  speed <= 30 || speed >= 95 ? true : FALSE
-  speed = 50 ? FALSE : true
+	speed > 60 || speed < 40 ? true : false
+  speed <= 30 || speed >= 95 ? true : false
 end
 	
 
